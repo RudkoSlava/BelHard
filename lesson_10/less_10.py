69c4258103b8b3bb9bb8236664881c7246795999
Slava Rudko <1234567890cdz@gmail.com>
2024-11-30 01:39:54 +0300

    add new file

--- lesson_10/less_10.py
+++ lesson_10/less_10.py
@@ -2,7 +2,6 @@
 # content = file.read()
 # print(content)
 # file.close()
-import json
 
 ##################################
 #
@@ -34,7 +33,8 @@ import json
 #     json.dump(data, file)
 
 ##################################
-#
+#import json
+
 # with open("class_work.json", "r") as file:
 #     dict_json_data = json.load(file)
 # print(dict_json_data)
@@ -71,17 +71,77 @@ import json
 
 ##################################
 
-from pydantic import BaseModel
+#from pydantic import BaseModel
 
-class Product(BaseModel):
-    name: str
-    price: float
-    quantity: int
-def add_product(name, price, quantity):
-    return Product(name=name, price=price, quantity=quantity)
+# class Product(BaseModel):
+#     name: str
+#     price: float
+#     quantity: int
+# def add_product(name, price, quantity):
+#     return Product(name=name, price=price, quantity=quantity)
+#
+# product = {"name": "Milk", "price": 2, "qiantity": 25}
+# print(add_product("Milk", 2, 25))
 
-product = {"name": "Milk", "price": 2, "qiantity": 25}
-print(add_product("Milk", 2, 25))
+##################################
 
+# from pydantic import BaseModel, ValidationError
+# import json
 
+# class User(BaseModel):
+#     name: str
+#     age: int
+#     email: str
+#
+# def test(name, age, email):
+#     return User(name=name, age=age, email=email)
+#
+# person = User(name="Slava", age=28, email="1234567890cdz@gmail.com")
+#
+# json_data = person.json()
+#
+# with open("Persons.json", "w") as json_file:
+#     json_file.write(json_data)
+#
+# print("Данные успешно добавлены в 'Persons.json'")
 
+##################################
+
+# from pydantic import BaseModel, ValidationError
+# import json
+
+# class User(BaseModel):
+#     name: str
+#     age: int
+#     email: str
+# def test():
+#     user_input = {
+#         "name": input('Введите имя: '),
+#         "age": input('Введите возраст: '),
+#         "email": input('Введите email: ')
+#     }
+#
+#     try:
+#         user = User(**user_input)
+#         with open("Persons.json", 'w', encoding="UTF-8") as file:
+#             json.dump(user.dict(), file, indent=4)
+#         print("Данные успешно сохранены.")
+#     except ValidationError as e:
+#         print("Ошибка валидации данных.", e)
+#
+# test()
+
+##################################
+
+import csv
+
+data = [
+    ['name', 'age'],
+    ['Bob', 32],
+    ['Alex', 35]
+]
+
+with open('data.csv', 'w', newline='') as file:
+    csvwriter = csv.writer(file, delimiter=',')
+    for row in data:
+        csvwriter.writerow(row)
